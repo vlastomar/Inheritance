0930a468caa71488bd116da1d2ed52b368d4cd5b
VSM <vlad24@abv.bg>
2020-09-22 14:20:37 +0300

    Heero

---
+++ hero/Main.java
@@ -0,0 +1,10 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+    String name = "Gosho";
+    int level = 12;
+    BladeKnight bk = new BladeKnight(name, level);
+        System.out.println(bk.toString());
+    }
+}
